72a99ae5104575663ed91d7aeded19eccd34ee8e
itallopacheco <itallopss@dcomp.ufs.br>
2023-10-02 17:59:18 -0300

    :sparkle: primeiro commit

---
+++ src/test/java/com/oicapivara/gerenciadorprocessos/GerenciadorProcessosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oicapivara.gerenciadorprocessos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorProcessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
