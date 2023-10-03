d6a59651c141cd6c740e670e40851770b2464950
itallopacheco <itallopss@dcomp.ufs.br>
2023-10-03 20:18:05 -0300

    :construction: logica de diretorios

--- src/main/java/com/oicapivara/gerenciadorprocessos/documentos/DocumentoServiceImp.java
+++ src/main/java/com/oicapivara/gerenciadorprocessos/documentos/DocumentoServiceImp.java
@@ -5,15 +5,24 @@ import com.oicapivara.gerenciadorprocessos.processo.Processo;
 import com.oicapivara.gerenciadorprocessos.processo.ProcessoRepository;
 import jakarta.servlet.ServletContext;
 import jakarta.validation.Valid;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.print.Doc;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class DocumentoServiceImp implements DocumentoService{
@@ -23,46 +32,51 @@ public class DocumentoServiceImp implements DocumentoService{
     @Autowired
     ProcessoRepository processoRepository;
 
-    @Autowired
-    private ServletContext servletContext;
 
     @Value("${file.upload.directory}")
     private String baseDir;
 
-    private String getAbsPath(){
-        String workingDir = servletContext.getRealPath("/");
-        return workingDir + File.separator + baseDir;
-    }
 
 
     @Override
     public String upload(Long id, MultipartFile file) {
-        String filePath = getAbsPath() + file.getOriginalFilename();
         Optional<Processo> processoOptional = processoRepository.findById(id);
-        Processo processo = processoOptional.get();
-        if (processoOptional.isEmpty())throw new EntityNotFoundException("Processo não encontrado para o id: "+id);
+        Processo processo = processoOptional.orElseThrow( () -> new EntityNotFoundException("Processo não encontrado para o id: "+id));
 
-        Documento documento = documentoRepository.save(Documento.builder()
-                .nome(file.getOriginalFilename())
-                .extensao(file.getContentType())
-                .processo(processo)
-                .caminho(filePath).build()
-        );
+        String numProcesso = processo.getNumeroProcesso();
+        String originalFilename = file.getOriginalFilename();
+        String fileExtension = FilenameUtils.getExtension(originalFilename);
+        String uuid = UUID.randomUUID().toString();
 
-        processo.addDocumento(documento);
-        processoRepository.save(processo);
-
+        String destinationDirectory = baseDir + numProcesso;
+        try{
+            Files.createDirectories(Paths.get(destinationDirectory));
+        } catch (IOException e) {
+            throw new RuntimeException("Não foi possível criar o diretório de destino"+ e);
+        }
+        String destinationFilename = destinationDirectory + "/" + originalFilename + "_" + uuid + "." + fileExtension;
 
         try {
-            file.transferTo(new File(filePath));
+            Files.copy(file.getInputStream(),
+                    Path.of(destinationFilename),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            Documento documento = documentoRepository.save(Documento.builder()
+                    .nome(file.getOriginalFilename())
+                    .extensao(file.getContentType())
+                    .processo(processo)
+                    .caminho(destinationFilename).build()
+            );
+
+            processo.addDocumento(documento);
+            processoRepository.save(processo);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
 
-        if (documento != null){
-            return "documento enviado com sucesso " + filePath;
-        }
-        return null;
+        return "documento enviado com sucesso " + destinationFilename;
+
     }
 }
